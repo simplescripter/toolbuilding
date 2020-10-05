21277f19ac8495d2783c2aa69f342cddb9201674
Shawn Stugart <simplescripter@gmail.com>
2020-10-05 14:56:39 -0600

    Create 1.6.ps1

---
+++ 1.6.ps1
@@ -0,0 +1,22 @@
+# Can declare a mandatory parameter with [Parameter(Mandatory=$true)]
+
+Function Test-Net {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true)]
+        [string[]]$computerName,
+
+        [int]$port = 135
+    )
+    ForEach ($computer in $computerName){
+        Write-Verbose "Now testing $computer"
+        $pingResult = Test-NetConnection -ComputerName $computer -InformationLevel Quiet -WarningAction SilentlyContinue
+        If($pingResult){
+            Write-Output $pingResult
+        }Else{
+            Write-Verbose "Ping failed. Checking port..."
+            $portResult = Test-NetConnection -ComputerName $computer -InformationLevel Quiet -WarningAction SilentlyContinue -Port $port
+            Write-Output $portResult
+        }
+    }
+}
