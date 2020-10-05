7680f935e061cdaa68a812f4f55cd0a07222b808
Shawn Stugart <simplescripter@gmail.com>
2020-10-05 14:53:12 -0600

    Update 1.5.ps1

--- 1.5.ps1
+++ 1.5.ps1
@@ -1,14 +1,23 @@
 # With static values, we need to change the computer name and port number in the script
 
-Param (
-    [string]$computerName,
-    [int]$port = 135
-)
+Function Test-Net {
+    [CmdletBinding()]
+    Param(
+        [string[]]$computerName,
 
-$pingResult = Test-NetConnection -ComputerName $computerName -InformationLevel Quiet -WarningAction SilentlyContinue
-If($pingResult){
-    Write-Output $pingResult
-}Else{
-    $portResult = Test-NetConnection -ComputerName $computerName -InformationLevel Quiet -WarningAction SilentlyContinue -Port 135
-    Write-Output $portResult
+        [int]$port = 135
+    )
+    ForEach ($computer in $computerName){
+        Write-Verbose "Now testing $computer"
+        $pingResult = Test-NetConnection -ComputerName $computer -InformationLevel Quiet -WarningAction SilentlyContinue
+        If($pingResult){
+            Write-Output $pingResult
+        }Else{
+            Write-Verbose "Ping failed. Checking port..."
+            $portResult = Test-NetConnection -ComputerName $computer -InformationLevel Quiet -WarningAction SilentlyContinue -Port $port
+            Write-Output $portResult
+        }
+    }
 }
+
+Test-Net LON-DC1,LON-SVR1
