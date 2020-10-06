f920d1aaf382121b8d3851e019d08ba7042bc4ff
Shawn Stugart <simplescripter@gmail.com>
2020-10-06 10:50:16 -0600

    upload 1.10

---
+++ 1.10.ps1
@@ -0,0 +1,36 @@
+
+
+Function Test-Net {
+    [CmdletBinding(DefaultParameterSetName="Computer")]
+    Param(
+        [Parameter(Mandatory=$true,HelpMessage="Enter the name of a computer to check connectivity to",
+        ParameterSetName="Computer")]
+        [Alias("serverName","hostName")]
+        [ValidateCount(1,64)]   # maybe we want to limit the number of computers in a single scan
+        [ValidateLength(1,15)]  # and we don't want computer names longer than 15 characters
+        [string[]]$computerName,
+
+        [Parameter(Mandatory=$true,HelpMessage="Enter the IP of a computer to check connectivity to",
+        ParameterSetName="IP")]
+        [string[]]$IP,  
+                        
+
+        [int]$port = 135
+    )
+    
+
+    If($PSBoundParameters.ContainsKey('IP')){
+        $computerName = $IP 
+    }
+    ForEach ($computer in $computerName){
+        Write-Verbose "Now testing $computer"
+        $pingResult = Test-NetConnection -ComputerName $computer -InformationLevel Quiet -WarningAction SilentlyContinue
+        If($pingResult){
+            Write-Output $pingResult
+        }Else{
+            Write-Verbose "Ping failed. Checking port..."
+            $portResult = Test-NetConnection -ComputerName $computer -InformationLevel Quiet -WarningAction SilentlyContinue -Port $port
+            Write-Output $portResult
+        }
+    }
+}
