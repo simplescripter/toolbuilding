4d99f9c50a51c5c9a8fbc4459d23015b13299f51
Shawn Stugart <simplescripter@gmail.com>
2020-10-06 13:39:19 -0600

    upload 1.16

---
+++ 1.16.ps1
@@ -0,0 +1,12 @@
+﻿# 1.16 Module manifests
+ 
+$params = @{
+    Path = 'C:\Users\Administrator.ADATUM\Documents\WindowsPowerShell\Modules\PSLib\PSLib.psd1'
+    RootModule = 'C:\Users\Administrator.ADATUM\Documents\WindowsPowerShell\Modules\PSLib\PSLib.psm1'
+    Author = 'Admin'
+    ModuleVersion = '1.0.0'
+    Description = "A library of internal Powershell functions"
+}
+New-ModuleManifest @params
+ 
+Test-ModuleManifest 'C:\Users\Administrator.ADATUM\Documents\WindowsPowerShell\Modules\PSLib\PSLib.psd1'
