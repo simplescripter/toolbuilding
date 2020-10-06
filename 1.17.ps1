51a5afb7ec3274f86402374a29851397c7946c0d
Shawn Stugart <simplescripter@gmail.com>
2020-10-06 13:49:33 -0600

    upload 1.17

---
+++ 1.17.ps1
@@ -0,0 +1,20 @@
+﻿# 1.17 Publishing modules internally
+
+Invoke-Command LonSvr1 {
+    New-Item -Name C:\PSRepo -ItemType Directory
+    New-SmbShare -Path C:\PSRepo -Name PSRepo -FullAccess Everyone
+}
+$networkShare = "\\LonSvr1\PSRepo"
+$repo = @{
+    Name = 'MyRepo'
+    SourceLocation = $networkShare
+    PublishLocation = $networkShare
+    InstallationPolicy = 'Trusted'
+}
+Register-PSRepository @repo
+Find-Module -Repository 'MyRepo'
+$publishModuleSplat = @{
+Repository = 'MyRepository'
+Path = '.\MyModule'
+}
+Publish-Module @publishModuleSplat
