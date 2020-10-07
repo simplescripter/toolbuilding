71ac586e412152972987d72ded597d55add9d21e
Shawn Stugart <simplescripter@gmail.com>
2020-10-07 15:17:22 -0600

    Update 2.4.ps1

--- 2.4.ps1
+++ 2.4.ps1
@@ -4,7 +4,6 @@ $params = @{
     TypeName = 'Microsoft.ActiveDirectory.Management.ADComputer'
     MemberType = 'ScriptProperty'
     MemberName = 'ComputerName'
-    Value = "{$this.Name}"
 }
 
-Update-TypeData @params
+Update-TypeData @params -Value {$this.Name}
