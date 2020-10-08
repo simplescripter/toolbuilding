24a14fa56010f098e7cbc64b1b66cdbf5616f167
Shawn Stugart <simplescripter@gmail.com>
2020-10-08 10:47:20 -0600

    Create 3.4.ps1

---
+++ 3.4.ps1
@@ -0,0 +1,5 @@
+﻿# 3.4 Example: Invoke-ReSTMethod with Intune
+
+$script = Invoke-WebRequest "https://raw.githubusercontent.com/microsoftgraph/powershell-intune-samples/master/ManagedDevices/ManagedDevices_Get.ps1"
+$script.Content | Out-File $env:TEMP\intune.ps1
+ise $env:TEMP\intune.ps1
