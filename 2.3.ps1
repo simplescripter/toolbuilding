c7a2c25c8bab995096e1b51718c5a20d9b6dd2bf
Shawn Stugart <simplescripter@gmail.com>
2020-10-08 10:06:33 -0600

    updates

--- 2.3.ps1
+++ 2.3.ps1
@@ -1,10 +1,23 @@
 ﻿# 2.3  Constructors, Overloads, and Enumerations
 
-[System.Collections.Arraylist]$collection = <list>
-$array = New-Object System.Collections.Arraylist
+# Arraylist example
 
-			#	-or-
-				
-$array = [System.Collections.ArrayList]::New
+[System.Collections.ArrayList]$computerList = 'LON-DC1','LON-SVR1','LON-CL1'
+Do {
+    [array]$list = $computerList
+    ForEach($computer in $list){
+        If(Test-Connection -ComputerName $computer -Count 1 -Quiet){
+            $computerList.Remove($computer)
+        }
+    }
+    If($computerList.Count -gt 0){Start-Sleep 5}
+}Until($computerList.Count -eq 0)
+Write-Host 'All systems in $computerList have been contacted.' -ForegroundColor DarkCyan -BackgroundColor Yellow
+
+# can also use $array = New-Object System.Collections.Arraylist or $array = [System.Collections.ArrayList]::New
+
+# enumerations examples
 
 [System.ConsoleColor].GetMembers() | Select Name
+
+[Net.SecurityProtocolType]::Tls13
--- 2.4.ps1
+++ 2.4.ps1
@@ -1,5 +1,25 @@
 # 2.4 Add-Type and Update-TypeData
 
+# Demo 1
+
+# The Add-Type cmdlet lets you define a Microsoft .NET Framework class in your Windows PowerShell session. 
+# You can specify the type by specifying an existing assembly or source code files, or you can specify the 
+# source code inline or saved in a variable
+
+# Demo 1: Winforms
+
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.Application]::EnableVisualStyles()
+
+$Form                            = New-Object system.Windows.Forms.Form
+$Form.ClientSize                 = New-Object System.Drawing.Point(400,400)
+$Form.text                       = "Form"
+$Form.TopMost                    = $false
+
+[void]$Form.ShowDialog()
+
+# Demo 2
+
 $params = @{
     TypeName = 'Microsoft.ActiveDirectory.Management.ADComputer'
     MemberType = 'ScriptProperty'
