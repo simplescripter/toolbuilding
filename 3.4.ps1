0fb89c5365aca624786b2ab1648e372353ebfaed
Shawn Stugart <simplescripter@gmail.com>
2020-10-09 15:09:03 -0600

    Update 3.4.ps1

--- 3.4.ps1
+++ 3.4.ps1
@@ -1,4 +1,4 @@
-﻿# 3.4 Example: Invoke-ReSTMethod with Intune
+# 3.4 Example: Invoke-ReSTMethod with Intune
 
 $script = Invoke-WebRequest "https://raw.githubusercontent.com/microsoftgraph/powershell-intune-samples/master/ManagedDevices/ManagedDevices_Get.ps1"
 $script.Content | Out-File $env:TEMP\intune.ps1
