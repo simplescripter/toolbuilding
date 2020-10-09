f3a866227a36e5d0dfa0e411e9257c3c3d8b2bd4
Shawn Stugart <simplescripter@gmail.com>
2020-10-09 14:51:08 -0600

    Create 4.5.ps1

---
+++ 4.5.ps1
@@ -0,0 +1,36 @@
+﻿# 4.5 Introduction to PowerShell GUIs
+
+Function Get-Calendar {  
+
+    $form = New-Object Windows.Forms.Form  
+    $form.Text = "Select the date ranges from the Calendar and press Enter"  
+    $form.Size = New-Object Drawing.Size @(656,639)  
+
+    # Create a "hidden" SelectButton to handle Enter Key 
+
+    $btnSelect = New-Object System.Windows.Forms.Button 
+    $btnSelect.Size = "1,1" 
+    $btnSelect.add_Click({  
+    $form.Close()  
+    })  
+    $form.Controls.Add($btnSelect )  
+    $form.AcceptButton =  $btnSelect 
+
+    # Add Calendar  
+
+    $calendar = New-Object System.Windows.Forms.MonthCalendar  
+    $calendar.ShowWeekNumbers = $false  
+    $calendar.MaxSelectionCount = 356 
+    $calendar.Dock = 'Fill'  
+    $form.Controls.Add($calendar)  
+
+    # Display the form 
+
+    $form.Add_Shown({$form.Activate()})   
+    [void]$form.Showdialog()  
+
+    # Return Start and End date  
+
+    Return $calendar.SelectionRange
+
+}  
