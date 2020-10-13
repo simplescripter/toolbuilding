bb289963fc6ecf05aacce65a5e41af1825540d3f
Shawn Stugart <simplescripter@gmail.com>
2020-10-13 10:22:32 -0600

    create scripts

--- 6.1.1.ps1
+++ 6.1.1.ps1
@@ -1,12 +1,26 @@
-# 5.3 Trapping Run-Time Errors with Try/Catch
+﻿# 5.8.1 Write-Debug
 
-# Function to fetch the ReleaseId value from one or more Windows systems using WMI and the STDREG provider
-# (Windows clients don't have PowerShell remoting enabled by default, but WMI may be available)
+Function Write-ToLog {
+    Param (
+        [string]$logPath = 'D:\scratchspace\log.csv', # We've parameterized $logPath
+
+        [string]$logData
+    )
+
+    $properties = @{
+        Date = (Get-Date)
+        Data = $logData
+    }
+    New-Object -TypeName psobject -Property $properties | Export-Csv -NoTypeInformation -Append -Path $logPath 
+}
+
 Function Get-OSReleaseID {
     [CmdletBinding()]
     Param (
         [Parameter(ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]
-        [string[]]$computerName = 'localhost'
+        [string[]]$computerName = 'localhost',
+
+        [switch]$logIt
     )
     Process {
         $HKLM = 2147483650
@@ -17,8 +31,14 @@ Function Get-OSReleaseID {
             $ok = $true
             Try{
                 $wmi = [wmiclass]"\\$computer\root\default:stdRegProv"
+                If($logIt){
+                    Write-ToLog "$computer reachable over WMI"
+                }
             }Catch{
                 $ok = $false
+                If($logIt){
+                    Write-ToLog "FAILED connecting to $computer over WMI"
+                }
             }
             If($ok){
                 $releaseIDValue = ($wmi.GetStringValue($HKLM,$key,$releaseID)).sValue
@@ -30,6 +50,9 @@ Function Get-OSReleaseID {
                 }
                 $return = New-Object -TypeName PSObject -Property $properties
                 Write-Output $return
+                If($logIt){
+                    Write-ToLog $return
+                }
             }Else{
                 $properties = @{
                     ComputerName = $computer
@@ -38,9 +61,12 @@ Function Get-OSReleaseID {
                 }
                 $return = New-Object -TypeName PSObject -Property $properties
                 Write-Output $return
+                If($logIt){
+                    Write-ToLog $return
+                }
             }
         }
     }
 }
 
-
+Get-OSReleaseID -computerName LonSVR1,LonDC1 -logIt # the errors don't get logged as we expect
