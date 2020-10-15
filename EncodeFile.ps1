61843ead64e0ef1618f18fc4f8a38e0216ef752e
Shawn Stugart <simplescripter@gmail.com>
2020-10-14 18:26:55 -0600

    updates

---
+++ EncodeFile.ps1
@@ -0,0 +1,20 @@
+﻿Function Encode-File {
+    [CmdletBinding()]
+    Param(
+        [Parameter(ValueFromPipeline=$true,Mandatory=$true)]
+        [string[]]$file
+    )
+    Begin{
+    $html1 = @"
+            <td><img border=0 alt="image"
+            src="data:image/jpeg;base64,
+"@
+    $html2 = @"
+            "></td>
+"@ 
+    }
+    Process{
+        $Base64 = [convert]::ToBase64String((Get-Content $file -Encoding Byte))
+            Write-Output $html1,$Base64,$html2
+    }
+}
