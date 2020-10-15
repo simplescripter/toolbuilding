a2a74342685d67dec3f82e5646443d00a0511a2c
Shawn Stugart <simplescripter@gmail.com>
2020-10-14 20:45:57 -0600

    create

--- 7.7.1.ps1
+++ 7.7.1.ps1
@@ -1,4 +1,8 @@
-﻿Get-Item -Path '\\LON-DC1\Netlogon' | Out-Null
+﻿# 7.7.1 Working with Non-structured Data: ConvertFrom-String
+
+# Credit to Don Jones and Jeffrey Hicks for the example
+
+Get-Item -Path '\\LonDC1\Netlogon' | Out-Null
 
 $netstatOutput = netstat -p tcp 
 $netstatOutput
@@ -8,4 +12,4 @@ $netstatConverted = ($netstatOutput[4..$netstatOutput.Count]).Trim() |
                                    -PropertyNames Proto,LocalAddress,ForeignAddress,State
 $netstatConverted
 
-$netstatConverted | Where-Object -Property 'ForeignAddress' -Like 'LON-DC1:*' 
+$netstatConverted | Where-Object -Property 'ForeignAddress' -Like 'LonDC1:*' 
--- 7.7.2.ps1
+++ 7.7.2.ps1
@@ -1,4 +1,8 @@
-﻿Get-Item -Path '\\LON-DC1\Netlogon' | Out-Null
+﻿# 7.7.2 Working with Non-structured Data: ConvertFrom-String Continued
+
+# Credit to Don Jones and Jeffrey Hicks for the example
+
+Get-Item -Path '\\LON-DC1\Netlogon' | Out-Null
 
 $netstatOutput = netstat -p tcp 
 $netstatOutput
@@ -8,4 +12,4 @@ $template = @"
 Active Connections
 
   Proto  Local Address          Foreign Address        State
-  {Proto*:TCP}    {LocalAddress:192.168.0.40:53119}     {ForeignAddress:msnbot-65-52-108-201:https}  {State:ESTABLISHED}  {Proto*:TCP}    {LocalAddress:192.168.0.40:64400}     {ForeignAddress:SVR01:microsoft-ds}     {State:ESTABLISHED}"@$netstatConverted = $netstatOutput | ConvertFrom-String -TemplateContent $template$netstatConverted$netstatConverted | Where-Object -Property 'ForeignAddress' -Like 'LON-DC1:*'
+  {Proto*:TCP}    {LocalAddress:192.168.0.40:53119}     {ForeignAddress:msnbot-65-52-108-201:https}  {State:ESTABLISHED}  {Proto*:TCP}    {LocalAddress:192.168.0.40:64400}     {ForeignAddress:SVR01:microsoft-ds}     {State:ESTABLISHED}"@$netstatConverted = $netstatOutput | ConvertFrom-String -TemplateContent $template$netstatConverted$netstatConverted | Where-Object -Property 'ForeignAddress' -Like 'LonDC1:*'
