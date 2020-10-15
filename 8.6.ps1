9d2c5809d4afd485b37a34ae5ff506a8bf1faad3
Shawn Stugart <simplescripter@gmail.com>
2020-10-15 10:07:07 -0600

    Create 8.6.ps1

---
+++ 8.6.ps1
@@ -0,0 +1,18 @@
+﻿# 8.6 Writing DSC Scripts
+
+Configuration FileServerConfig
+{
+    Node @('LonSVR1','LonDC1') {
+        WindowsFeature FileServer {
+            Name = 'FS-FileServer'
+            Ensure = 'Present'
+        }
+        WindowsFeature DataDedup {
+            Name = 'FS-Data-Deduplication'
+            Ensure = 'Present'
+        }
+    }
+}
+
+FileServerConfig -OutputPath C:\DscConfigs
+Start-DscConfiguration -Path C:\DscConfigs
