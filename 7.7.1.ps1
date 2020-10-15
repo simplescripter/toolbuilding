ebb51b400241458169c99abc2363d75c4120dabd
Shawn Stugart <simplescripter@gmail.com>
2020-10-14 20:42:06 -0600

    create

---
+++ 7.7.1.ps1
@@ -0,0 +1,11 @@
+﻿Get-Item -Path '\\LON-DC1\Netlogon' | Out-Null
+
+$netstatOutput = netstat -p tcp 
+$netstatOutput
+
+$netstatConverted = ($netstatOutput[4..$netstatOutput.Count]).Trim() | 
+                ConvertFrom-String -Delimiter '[ ]{2,}' `
+                                   -PropertyNames Proto,LocalAddress,ForeignAddress,State
+$netstatConverted
+
+$netstatConverted | Where-Object -Property 'ForeignAddress' -Like 'LON-DC1:*' 
---
+++ 7.7.2.ps1
@@ -0,0 +1,11 @@
+﻿Get-Item -Path '\\LON-DC1\Netlogon' | Out-Null
+
+$netstatOutput = netstat -p tcp 
+$netstatOutput
+
+$template = @"
+
+Active Connections
+
+  Proto  Local Address          Foreign Address        State
+  {Proto*:TCP}    {LocalAddress:192.168.0.40:53119}     {ForeignAddress:msnbot-65-52-108-201:https}  {State:ESTABLISHED}  {Proto*:TCP}    {LocalAddress:192.168.0.40:64400}     {ForeignAddress:SVR01:microsoft-ds}     {State:ESTABLISHED}"@$netstatConverted = $netstatOutput | ConvertFrom-String -TemplateContent $template$netstatConverted$netstatConverted | Where-Object -Property 'ForeignAddress' -Like 'LON-DC1:*'
