37d9f2e0010dd1850b67d082c755fabab7165020
Shawn Stugart <simplescripter@gmail.com>
2020-10-14 19:13:04 -0600

    updates

---
+++ 7.2.1.ps1
@@ -0,0 +1,23 @@
+﻿# 7.2.1 Importing and Searching XML
+
+# Read the content as strings with Get-Content and cast it as XML
+[xml]$xml = Get-Content C:\assay.xml
+
+# Display the XML document element
+$xml
+
+# Display the root <computers> element
+$xml.computers
+
+# Display the computer nodes
+$xml.computers.computer
+
+# Display the first computer node
+$xml.computers.computer[0]
+
+# Display the first computer's <sysinfo> element
+$xml.computers.computer[0].sysinfo
+
+# display the first computer's Version attribute
+$xml.computers.computer[0].sysinfo.Version
+
---
+++ 7.2.2.ps1
@@ -0,0 +1,19 @@
+﻿# 7.2.2 Importing and Searching XML Continued
+
+# Read the content as strings with Get-Content and cast it as XML
+[xml]$xml = Get-Content C:\assay.xml
+
+# select the single <computer> node having
+# the Name attribute of LonDC1
+# (This is an XPath query)
+$node = $xml.SelectSingleNode('//computer[@name="LonDC1"]')
+
+# display the selected node
+$node.InnerXml
+
+# select all <sysinfo> nodes that
+# follow the <computer> node having the
+# Name attribute of LonDC1. Notice that the
+# XPath query follows the hierarchy of the XML.
+$nodes = $xml.SelectNodes('//computer[@name="LonDC1"]/sysinfo')
+$nodes
