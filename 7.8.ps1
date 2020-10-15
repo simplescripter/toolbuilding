fb2b6cff6bdbe09f634c8b6fc5895ec4f5ee8fea
Shawn Stugart <simplescripter@gmail.com>
2020-10-14 20:17:14 -0600

    Create 7.8.ps1

---
+++ 7.8.ps1
@@ -0,0 +1,9 @@
+﻿# 7.8 Working with Non-structured Data: Convert-String and Regex Groupings
+
+# Convert-String formats a string to match examples
+
+'Richie Tozier', 'Beverly Marsh' | Convert-String -Example 'John Doe=Doe, J.'
+
+# Regex groupings use parenthesis
+
+'Richie Tozier' -replace '(\w+) (\w+)', '$2, $1'
