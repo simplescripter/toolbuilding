85cc4a38ff4e08e7356d6f475830eb7c0d2df478
Shawn Stugart <simplescripter@gmail.com>
2020-10-15 07:30:29 -0600

    Create 8.4.ps1

---
+++ 8.4.ps1
@@ -0,0 +1,16 @@
+﻿# 8.4 Demo: LCM Configuration
+
+[DSCLocalConfigurationManager()]
+Configuration LCMConfig
+{
+    Node @('LonCL1','LonSVR1','LonDC1') {
+        Settings {
+            ConfigurationMode = 'ApplyAndAutoCorrect'
+            ConfigurationModeFrequencyMins = 30
+            RefreshMode = 'Push'
+        }
+    }
+}
+
+LCMConfig -OutputPath C:\LCMConfigs
+Set-DscLocalConfigurationManager -Path C:\LCMConfigs
