bbf945fee5c37a83f9ff3202957e54afcd5e5c4e
Shawn Stugart <simplescripter@gmail.com>
2020-10-16 11:35:32 -0600

    Create 10.5.ps1

---
+++ 10.5.ps1
@@ -0,0 +1,13 @@
+﻿# 10.5 .Net Runspaces
+
+Find-Command Invoke-Parallel
+
+Install-Module WFTools
+
+1..50 | Invoke-Parallel -Throttle 32 -ScriptBlock {
+    If(Test-Connection "172.16.0.$_" -Count 1 -Quiet){
+        Write-Host "172.16.0.$_`n" -ForegroundColor Green -NoNewline
+    }Else{
+        Write-Host "172.16.0.$_`n" -ForegroundColor Red -NoNewline
+    }
+}
