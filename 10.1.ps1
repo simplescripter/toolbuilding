020c49e4e42cd3ef7ea61fe4c1eb83d93ad22525
Shawn Stugart <simplescripter@gmail.com>
2020-10-16 10:50:48 -0600

    Create 10.1.ps1

---
+++ 10.1.ps1
@@ -0,0 +1,15 @@
+﻿# 10.1 Invoke-Command
+
+$computers = 'LonCL1','LonSVR1','LonDC1'
+help Invoke-Command -Parameter ThrottleLimit
+
+# The -ComputerName parameter is best for ad-hoc commands
+Invoke-Command -ComputerName $computers {$log = Get-EventLog -LogName Security -Newest 100;$log}
+Invoke-Command -ComputerName $computers {$log} # $log is empty because a session wasn't used
+
+$sessionAll = New-PSSession -ComputerName $computers
+Invoke-Command -Session $sessionAll {$log = Get-EventLog -LogName Security -Newest 100;$log}
+Invoke-Command -Session $sessionAll {$log} # $log variable is maintained in the session
+
+Invoke-Command -Session $sessionAll {$log} | Sort PSComputerName
+Invoke-Command -Session $sessionAll {$log} | Sort PSComputerName | Format-Table -GroupBy PSComputerName
