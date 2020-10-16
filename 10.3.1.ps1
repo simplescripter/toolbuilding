d644d5d7733295db4eb1e8d4c0411af05f9eff72
Shawn Stugart <simplescripter@gmail.com>
2020-10-16 11:11:42 -0600

    create

---
+++ 10.3.1.ps1
@@ -0,0 +1,26 @@
+﻿# 10.3.1 Workflows
+
+WorkFlow Test-Parallel {
+    "First line executes"
+    Parallel{
+        sleep 2
+	    1
+	    2
+        sleep 2
+	    3
+	    4
+        sleep 2
+	    5
+	    6
+        sleep 2
+	    7
+	    8
+	    9
+	    Sequence{
+		    "A"
+		    "B"
+	    }
+    }
+}
+
+Test-Parallel
---
+++ 10.3.2.ps1
@@ -0,0 +1,23 @@
+﻿# 10.3.2 Workflows Continued
+
+Workflow PingSweep{
+    $computers = @()
+    For ($i = 1; $i -le 50; $i++){$computers += "172.16.0.$i"}
+    ForEach -Parallel ($computer in $computers){
+        If(Test-NetConnection -computerName $computer -InformationLevel Quiet -WarningAction SilentlyContinue){
+            Write-Output -InputObject "$computer,ONLINE"
+        }else{
+            Write-Output -InputObject "$computer,OFFLINE"
+        }
+    }
+}
+
+PingSweep | ForEach{
+    $ip = $_.Split(",")[0]
+    $status = $_.Split(",")[1]
+    If($status -eq 'ONLINE'){
+        Write-Host "$ip" -ForegroundColor Green
+    }Else{
+        Write-Host "$ip" -ForegroundColor Red
+    }
+}
