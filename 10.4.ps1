c6c1f894ce2a29a520454c4b82943ed7cc3fe305
Shawn Stugart <simplescripter@gmail.com>
2020-10-16 11:26:39 -0600

    Create 10.4.ps1

---
+++ 10.4.ps1
@@ -0,0 +1,11 @@
+﻿# 10.4 PowerShell 7+ ForEach-Object -Parallel
+
+#requires -version 7
+
+1..50 | ForEach-Object -Parallel {
+    If(Test-NetConnection -computerName "172.16.0.$_" -InformationLevel Quiet -WarningAction SilentlyContinue){
+            Write-Host "172.16.0.$_`n" -ForegroundColor Green -NoNewline
+    }Else{
+        Write-Host "172.16.0.$_`n" -ForegroundColor Red -NoNewline
+    }
+}
