9f70db92799a3d6ce2ed35f332021e09799b9d06
Shawn Stugart <simplescripter@gmail.com>
2020-10-15 19:00:44 -0600

    Create 9.4.ps1

---
+++ 9.4.ps1
@@ -0,0 +1,13 @@
+﻿# 9.4 Registering and Using JEA
+
+$managedEndpoint = 'LonSVR1'
+$configName = 'adatum.generalserver.level1'
+$jeaModulePath = 'Program Files\WindowsPowerShell\Modules\AdatumJEA'
+$rcFolder = 'RoleCapabilities'
+$jeaSessionConfigName = 'SampleJEAConfig.pssc'
+$jeaSessionConfigFile = Join-Path "c:\$jeaModulePath\$rcFolder" $jeaSessionConfigName
+Invoke-Command -ComputerName $managedEndpoint -ScriptBlock {
+    Register-PSSessionConfiguration -Path $using:jeaSessionConfigFile -Name $using:configName}
+
+$creds = Get-Credential -Message "Enter your credentials" -UserName 'Adatum\Dante'
+Enter-PSSession -ComputerName $managedEndpoint -ConfigurationName $configName -Credential $creds
